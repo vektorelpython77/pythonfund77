b766248b4804703dd6df9ad2910eb88155c32601
vektorelpython28 <vektorelpython28@gmail.com>
2023-04-09 09:03:38 +0000

    09042023

--- Egzersiz/ahmet/03_01_ifelse.py
+++ Egzersiz/ahmet/03_01_ifelse.py
@@ -3,20 +3,3 @@
 iki açısı girilmiş olan
 bir üçgenin tipini tespit eden programı yazınız
 """
-x=(input("1. açıyı giriniz."))
-y=(input("2. açıyı giriniz."))
-k=180-(x+y)
-if acibir and acibir.isdigit() and aciiki and aciiki.isdigit():
-
-
-    x=int(x)
-    y=int(y)
-
-if(x == y== k== 60):
-    print("Eşkenardır")
-elif(x==y or x==y or x==y):
-    print("İkizkenar Üçgendir")
-elif(k>90):
-    print("Geniş kenar üçgen")
-else:
-    print("girdiginiz deger yanlıs.")
--- Egzersiz/suleyman/03_01_ifelse.py
+++ Egzersiz/suleyman/03_01_ifelse.py
@@ -5,7 +5,6 @@ bir üçgenin tipini tespit eden programı yazınız
 """
 A=input("1. açı:");print(A.isdigit())
 B=input("2. açı:");print(B.isdigit())
-C=180-A.isdigit()-B.isdigit();print(C)
 if (A.isdigit()==B.isdigit()==C==60):
     print("Eşkenar Üçgen")
     print("Çeşit Üçgen")
