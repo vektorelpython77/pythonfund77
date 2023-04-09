a84672f3bd5771f29b8ced1547ae3fbd908ed748
kat5bilgsayarpython <86150398+vektorelpython28@users.noreply.github.com>
2023-04-09 13:17:20 +0300

    09042023

---
+++ 09042023Odev.py
@@ -0,0 +1,26 @@
+"""
+1. Aşağıdaki şekli ekrana yazdıran python kodunu yazınız
+#
+# # 
+# # # 
+# # # # 
+# # # # # 
+# # # # # #
+# # # # # 
+# # # # 
+# # # 
+# # 
+#
+"""
+
+"""
+2. 
+1 ile 150 sayıları arasındaki sayılardan 15'e tam bölünebilen 
+sayıların listesini ekrana yazdıran python kodunu yazınız
+"""
+
+"""
+3.
+fibanocci serisinde verilen sınıra kadar seri elemanlarını ekrana 
+yazdıran programı yazınız
+"""
