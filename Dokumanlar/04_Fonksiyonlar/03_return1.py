18eed114ac2ca25ec96d42ad9e0af5aa461f186c
vektorelpython28 <vektorelpython28@gmail.com>
2023-04-30 08:17:58 +0000

    30042023_2

--- Dokumanlar/04_Fonksiyonlar/03_return1.py
+++ Dokumanlar/04_Fonksiyonlar/03_return1.py
@@ -6,10 +6,45 @@ yield
 def fonk1(a,b):
     print(a+b)
 
-def fonk2(a,b):
-    return a+b
-    print("ÇALIŞMAZ")
+# def fonk2(a,b):
+#     return a+b
+#     print("ÇALIŞMAZ")
 
-fonk1(2,3)
-print(fonk1(2,3)) #None
-print(fonk2(2,3)) #5 çünkü return sayıları dışarı gönderdi
+# fonk1(2,3)
+# print(fonk1(2,3)) #None
+# print(fonk2(2,3)) #5 çünkü return sayıları dışarı gönderdi
+
+
+# def fonk(a,b):
+#     return a,b,a+b
+
+# print(fonk(2,3))
+# sonuc = fonk(2,3) # (2, 3, 5)
+# print(f"{sonuc[0]} + {sonuc[1]} = {sonuc[2]}")
+
+def fonk(metin,sayi):
+    import random
+    metin = list(metin)
+    for i in range(sayi):
+        random.shuffle(metin)
+        yield "".join(metin)
+
+print(fonk("Galatasaray",5)) # <generator object fonk at 0x7f93a6151030>
+# print(range(1,2)) # range(1, 2)
+# print(*range(1,5))  # 1 2 3 4
+# print(*fonk("Galatasaray",5),sep="\n")
+
+for item in fonk("Galatasaray",5):
+    print(item)
+
+from string import ascii_lowercase,ascii_uppercase,digits,punctuation
+print(ascii_lowercase,ascii_uppercase,digits,punctuation,sep="\n")
+import random
+print(random.choice(ascii_lowercase))
+
+"""
+abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+0123456789
+!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+"""
--- Egzersiz/alimert/04_04_Egzersiz2.py
+++ Egzersiz/alimert/04_04_Egzersiz2.py
@@ -1,10 +1,5 @@
-import os
-filename = "04_04_Egzersiz2"
-liste = os.listdir("Egzersiz/")
-for item in liste:
-    with open(f"Egzersiz/{item}/{filename}.py","w+",encoding="UTF-8") as dosya:
-        metin = """
-\"\"\"
+
+"""
 istenilen uzunlukta ve aşağıdaki kriterleri sağlayan şifre üreten
 fonksiyonu yazınız
 1. en az bir küçük harf olmalıdır
@@ -16,11 +11,5 @@ from string import ascii_lowercase,ascii_uppercase,digits,punctuation
 import random as rnd
 
 yukarıdaki kütüphanelerden faydalanabilirsiniz.
-\"\"\"
-
 """
-        dosya.write(metin)
-# TODO veri tabanı bağlantrısı
 
-
-# https://codeshare.io/eVn8ml
