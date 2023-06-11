7db294e6efaf89017bf029e1dc0a93416f4a3c0f
kat5bilgsayarpython <86150398+vektorelpython28@users.noreply.github.com>
2023-06-11 10:41:43 +0300

    Co-authored-by: SuleymanDemir1980 <SuleymanDemir1980@users.noreply.github.com> Co-authored-by: hakanuslumy45 <hakanuslumy45@users.noreply.github.com>

--- Dokumanlar/08_ModulvePaketler/cagir.py
+++ Dokumanlar/08_ModulvePaketler/cagir.py
@@ -1,4 +1,12 @@
-from paket import Paket
-Paket()
-from paket.paket2 import Paket2
-Paket2()
+# from paket import Paket
+# Paket()
+# from paket.paket2 import Paket2
+# Paket2()
+##############################
+# import os
+# os.path
+##############################
+# from os import path,environ
+# path.join
+# environ.update
+##############################
